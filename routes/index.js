d6035430a097ca010dfed2db0eefd78d3e414c40
malachirewane <malachi.rewane@ontariotechu.net>
2025-11-07 02:23:49 -0400

    adjusting routes for js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* Home Page */
+// Home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* About Page */
+// About page
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
 });
 
-/* Projects Page */
+// Projects page
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
-/* Contact Page */
+// Contact page
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
 });
